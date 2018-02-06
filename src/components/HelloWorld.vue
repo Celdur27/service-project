b34aca6ab7c4e3b04ce686300b9531c209ee6b68
bolewski <bolewski.mateusz@gmail.com>
2018-02-06 14:28:04 +0100

    Added CSS and filter
    Implemented filter to filter data with active and inactive state, added CSS to style services

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="hello">
-    <ul>
-      <li v-for="service in services">
-        <service :service="service"></service>
-      </li>
-    </ul>
+    <div class="header"></div>
+    <div class="content">
+      <div class="content--navbar">
+        <ul>
+          <li>Booking</li>
+          <li>Specialists</li>
+          <li>Users</li>
+          <li>Locations</li>
+          <li class="active">Services</li>
+        </ul>
+      </div>
+      <div class="content--services">
+        <div class="content--services__filter">
+          <ul>
+            <li v-on:click="changeState(1)">Active</li>
+            <li v-on:click="changeState(2)">Inactive</li>
+            <li v-on:click="changeState(0)">All</li>
+          </ul>
+          <button>ADD NEW SERVICE</button>
+        </div>
+        <div class="content--services__list">
+          <ul>
+            <li v-for="service in serviceList">
+              <service :service="service"></service>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,11 +43,34 @@ export default {
   ]),
   data () {
     return {
-      msg: 'Test Vue APP'
+      currentState: 0, 
+      serviceList: []
     }
   },
   components: {
     service
+  },
+  mounted() {
+    this.serviceList = this.services
+  },
+  methods: {
+    changeState(state) {
+      switch(state) {
+        case 0:
+          this.serviceList = this.services;
+          break;
+        case 1:
+          this.serviceList = this.services.filter(({active}) => {
+            return active;
+          });
+          break;
+        case 2:
+        this.serviceList = this.services.filter(({active}) => {
+            return !active;
+          });
+          break;
+      }
+    }
   }
 }
 </script>
@@ -44,11 +91,54 @@ li {
 a {
   color: #42b983;
 }
-.hello {
-  border: 1px solid grey;
-  border-radius: 10px;
-  width: 70%;
-  margin: auto;
+.header {
+  background-color: #0b2d68;
+  height: 50px;
+  width: 100%;
+}
+.content {
   display: flex;
+  flex: 1;
+}
+.content--navbar {
+  text-align: center;
+  background-color: #166bff;
+  color: #a8adb5;
+  width: 15%;
+}
+.content--navbar .active {
+  color: white;
+}
+.content--navbar li {
+  display: block;
+}
+.content--services__filter {
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+}
+.content--services__filter li {
+  cursor: pointer;
+  color: #166bff;
+}
+.content--services__filter button {
+  background-color: white;
+  border: none;
+  cursor: pointer;
+  color: #166bff;
+}
+.content--services__filter button:hover {
+  background-color: white;
+  border: none;
+  cursor: pointer;
+  color: black;
+}
+.content--services__list {
+  display: flex;
+}
+.hello {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
 }
 </style>
--- src/components/service.vue
+++ src/components/service.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="service">
-        <div class="service-data">
+        <div class="service--data">
             <h2>{{ service.name }}</h2>
-            <p>{{ service.subtitle }}</p>
-            <p>Booking Count: {{ service.booking_count }}</p>
+            <span class="service--data__description">{{ service.subtitle }}</span>
+            <span class="service--data__booking">Booking Count: {{ service.booking_count }}</span>
         </div>
-        <div class="service-images">
-            <img v-bind:src="'../assets/img/' + service.blueIconUrl" />
-            <img v-bind:src="'../assets/img/' + service.imageUrl" />
+        <div class="service--images">
+            <!--<img class="service--images__icon" v-bind:src="'../assets/img/' + service.blueIconUrl" />-->
+            <img class="service--images__img" :src="imageUrl" />
         </div>
     </div>
 </template>
@@ -22,6 +22,16 @@ export default {
     return {
       msg: 'Test Vue APP'
     }
+  },
+  computed: {
+      imageUrl() {
+          if(this.service.imageUrl) {
+              return '../assets/img/'+this.service.imageUrl
+          }
+          else {
+              return '../assets/img/24service.png'
+          }
+      }
   }
 }
 </script>
@@ -43,11 +53,40 @@ a {
     background-color: white;
     border: 1px solid;
     border-radius: 5px;
-    padding: 10px;
+    padding: 0 10px;
     box-shadow: 5px 10px #cddbf4;
-    width: 500px;
-    height: 200px;
+    width: 700px;
     margin-bottom: 20px;
     text-align: left;
+    display: flex;
+}
+.service .service--data {
+    width: 70%;
+}
+.service .service--data h2 {
+    color: #588fef;
+    font-weight: normal;
+    margin-bottom: 10px;
+}
+.service .service--data span {
+    display: block;
+    margin-bottom: 10px;
+}
+.service .service--data__description {
+    font-weight: 600;
+}
+.service .service--data__booking {
+    font-size: 14px;
+    color: grey;
+}
+.service--images__img {
+    width: 100%;
+}
+.service--images__img img {
+    width: 100%;
+}
+.service--images__icon {
+    z-index: 1;
+    position: relative;
 }
 </style>
